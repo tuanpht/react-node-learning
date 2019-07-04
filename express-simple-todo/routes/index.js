4219ed9d2ce3a508badbc534f6a3e86ff61af6e7
Tuan Pham <tuanpht.online@gmail.com>
2019-07-04 17:39:10 +0700

    Add error handler

--- express-simple-todo/routes/index.js
+++ express-simple-todo/routes/index.js
@@ -3,7 +3,29 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send({ title: 'Express' });
+});
+
+// Test error
+router.get('/error', function(req, res, next) {
+  throw new Error('Error occurred');
+});
+
+function asyncFunction() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      reject(new Error('Async error'));
+    }, 500);
+  });
+}
+
+// Test async error
+router.get('/async-error', async function(req, res, next) {
+  try {
+    await asyncFunction();
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
